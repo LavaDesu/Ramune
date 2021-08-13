77259721663913715a302bd99a6dd8563c33522b
LavaDesu <me@lava.moe>
2021-08-13 13:49:26 +0700

    chore(lint): use tsconfig.all.json for eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,7 +5,7 @@ module.exports = {
     },
     "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "project": ["tsconfig.json"],
+        "project": ["tsconfig.all.json"],
         "sourceType": "module",
         "tsconfigRootDir": __dirname
     },
