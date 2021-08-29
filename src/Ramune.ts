45ef09babcf94dad49a9d4fe8c0c5d4087cff45f
LavaDesu <me@lava.moe>
2021-08-29 12:25:40 +0700

    fix: re-export some missing modules
    - Errors
    - Structures/Base
    - Structures/Beatmap

--- src/Ramune.ts
+++ src/Ramune.ts
@@ -5,7 +5,11 @@ export * from "./Clients/UserClient";
 export * from "./Bucket";
 export * from "./Endpoints";
 export * from "./Enums";
+export * from "./Errors";
 export * from "./RequestHandler";
+
+export * from "./Structures/Base";
+export * from "./Structures/Beatmap";
 export * from "./Structures/User";
 
 export * from "./Util/Collection";
