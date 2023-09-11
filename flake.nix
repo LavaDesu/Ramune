352b33153d53ad6e01803fcd3d5e64f6de48f673
LavaDesu <me@lava.moe>
2023-09-11 14:07:07 +0700

    chore(flake): bump inputs and use nodejs 18

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in rec {
       devShell = pkgs.mkShell {
-        buildInputs = with pkgs; [ nodejs-14_x ];
+        buildInputs = with pkgs; [ nodejs-18_x ];
         shellHook = ''
           export PATH="$(readlink -f ./node_modules/.bin):$PATH"
         '';
